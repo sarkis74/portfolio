0e18e2f9647854565c69e3cfe191015e20aae0a3
sarkis7412 <sarkis_aghazarian@yahoo.com>
2018-12-01 12:51:57 -0800

    latest

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//Node Modules
+//dependencies
+
+require('dotenv').config();
+const express = require('express');
+
+//app 
+const app = express();
+
+//PORT
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(' ./public'));
+
+app.get(' /hello', (request, response) => {
+    response.status(200).send('hello');
+});
+
+// app.get 
+
+app.use('*', (req, res) => {
+    res.send('sorry that didn\'t work');
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
